29b3b40f8b80aab24e7f95affc41fcfac375cfd9
unknown <moyanopaulas@gmail.com>
2022-01-25 11:36:01 -0300

    Agrego otro ejercicio de Hacker Rank

---
+++ PositivosNegativos.js
@@ -0,0 +1,24 @@
+https://www.hackerrank.com/challenges/plus-minus/problem
+
+function plusMinus(arr) {
+    let positivos = 0;
+    let negativos = 0;
+    let ceros = 0;
+    for ( let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positivos++
+        } else if (arr[i] < 0) {
+            negativos++
+        }
+        else {
+            ceros++
+        }
+    }
+    console.log (positivos/arr.length);
+    console.log (negativos/arr.length);
+    console.log (ceros/arr.length);
+}
+
+
+plusMinus([-4,-2,0,2,5,7,10]);
+
