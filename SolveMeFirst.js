6aa5ba64df4b8473f8679aea3fd14fe6a207e4ab
unknown <moyanopaulas@gmail.com>
2022-02-15 20:19:53 -0300

    Practicando 15 febrero HackerRank

---
+++ SolveMeFirst.js
@@ -0,0 +1,9 @@
+//https://www.hackerrank.com/challenges/solve-me-first/problem
+
+
+function solveMeFirst(a, b) {
+    let resultado = a + b  
+    return resultado 
+  }
+
+  let result = solveMeFirst(10,17)
