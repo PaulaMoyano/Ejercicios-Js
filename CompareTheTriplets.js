abe4ea11da0f3715a0fe4af932f04408be32dd25
unknown <moyanopaulas@gmail.com>
2022-02-15 20:55:44 -0300

    Practicando 15 de febrero HackerRank

---
+++ CompareTheTriplets.js
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+function compareTriplets(a, b) {
+    let Alice = 0
+    let Bob = 0
+    for ( let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            Alice++
+    } else if (a[i] < b[i]) {
+            Bob++
+    }
+    }
+    let array = [Alice,Bob]
+    return array
+    }
+    
+console.log(compareTriplets[1,2,3],[3,2,1])
